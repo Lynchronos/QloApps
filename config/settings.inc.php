bd87f06a0dcf05d34bb7d14d6a156165f828e195
Lynchronos <lynchronos@gmail.com>
2026-08-22 10:30:30 +0500

    fix: restore bootstrap config.inc.php and add Railway DB settings

---
+++ config/settings.inc.php
@@ -0,0 +1,15 @@
+<?php
+define('_DB_SERVER_', 'altaria.proxy.rlwy.net:40352');
+define('_DB_NAME_', 'railway');
+define('_DB_USER_', 'root');
+define('_DB_PASSWD_', 'GLChEWVQZVtCLInAKlgIKYDcgnThuMDE');
+define('_DB_PREFIX_', 'qlo_');
+define('_MYSQL_ENGINE_', 'InnoDB');
+define('_PS_CACHING_SYSTEM_', 'CacheFS');
+define('_PS_CACHE_ENABLED_', '0');
+define('_COOKIE_KEY_', '9876543210fedcba9876543210fedcba9876543210fedcba9876543210fedcba');
+define('_COOKIE_IV_', '9876543210fedcba');
+define('_PS_CREATION_DATE_', '2026-08-22');
+define('_PS_VERSION_', '1.6.0.0');
+define('_RIJNDAEL_KEY_', '98765432109876543210987654321098');
+define('_RIJNDAEL_IV_', '9876543210987654');
